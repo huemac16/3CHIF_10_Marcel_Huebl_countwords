a69e8259575e0a58f829bc17a2e794c5ceff6c0b
huemac16 <huemac16@htl-kaindorf.ac.at>
2019-04-02 09:17:16 +0200

    added some code to producer class

--- src/main/java/htl/huebl/bsp_countwords/FileProducer.java
+++ src/main/java/htl/huebl/bsp_countwords/FileProducer.java
@@ -1,14 +1,42 @@
-
 package htl.huebl.bsp_countwords;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class FileProducer extends Thread{
+public class FileProducer extends Thread {
+
+    private final MyQueue<Book> books;
+
+    public FileProducer(MyQueue<Book> books) {
+        this.books = books;
+    }
 
     @Override
     public void run() {
-        super.run(); //To change body of generated methods, choose Tools | Templates.
+
+        while (true) {
+            File[] fileList = new File("\\files").listFiles();
+            String[] texts = new String[fileList.length];
+
+            for (int i = 0; i < fileList.length; i++) {
+                String text = "";
+
+                try (BufferedReader br = new BufferedReader(new FileReader(fileList[i]))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        text += line;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                texts[i] = text;
+            }
+
+        }
     }
-    
-    
-    
+
 }
