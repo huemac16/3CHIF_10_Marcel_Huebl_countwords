b2ebc89728b572e62e47be5c0e2e54c758146030
huemac16 <huemac16@htl-kaindorf.ac.at>
2019-04-02 08:56:52 +0200

    added the files to the project

---
+++ src/main/java/htl/huebl/bsp_countwords/FileProducer.java
@@ -0,0 +1,14 @@
+
+package htl.huebl.bsp_countwords;
+
+
+public class FileProducer extends Thread{
+
+    @Override
+    public void run() {
+        super.run(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
